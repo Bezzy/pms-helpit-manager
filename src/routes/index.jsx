43549cac6225ea47ac5ee46f6aa9f7849bfa43de
bewwys <m.buzyarsest@gmail.com>
2024-06-09 11:44:02 +0200

    Adding parcours

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,67 +1,399 @@
 import { A } from "@solidjs/router";
 import Counter from "~/components/Counter";
+import {For, createSignal, Show} from "solid-js";
+
+// Data
+import ParcoursTable from "~/data/parcours_table";
 
 export default function Home() {
+
+  let AppState = {
+    CurrentSelectedParcours : 64,
+  }
+
+  let [CurrentSelectedParcoursId, setCurrentSelectedParcoursId] = createSignal(null);
+  let [CurrentSelectedParcoursTable, SetCurrentSelectedParcoursTable] = createSignal({
+    "ref": "undefined",
+    "date": "undefined",
+    "names": "undefined",
+    "users": "undefined",
+    "id": "undefined"
+  });
+
+  function SelectCurrentParcours(Id) {
+    for(let idx = 0; idx < ParcoursTable.length; idx++) {
+      if (ParcoursTable[idx].id === Id) {
+        SetCurrentSelectedParcoursTable(ParcoursTable[idx]);
+      }
+    }
+    setCurrentSelectedParcoursId(Id);
+  }
+
   return (
-    <main class="bg-[#757575]/[0.4] h-full pl-[8px] pr-[8px] pb-[8px] flex flex-col">
+    <main class="pt-[96px] bg-[#757575]/[0.4] h-full pl-[8px] pr-[8px] pb-[8px] flex flex-col">
         <h1 class="text-[24px]">Liste des parcours</h1>
-        <div class="div-that-is-useless-but-is-necessary-because-css-is-crap  w-[100%] flex flex-1 gap-[8px]">
-          <div class="bg-[#F6F6F6]/[0.5] rounded-[4px] flex flex-1 basis-1/2 overflow-y-scroll">
-            <div class="flex flex-col min-h-min">
-              <div class="pl-[8px] bg-blue-800 rounded-[4px] text-blue-200">
-                <h1>Réf: <span class="font-black">PC-478</span></h1>
-                <h1>Date d'ouverture du dossier: <span class="font-black">29-05-204</span></h1>
-                <h1>Bénéficiaire(s) impliqué(s): <span class="underline text-sky-500">Amina Diallo</span>, <span class="underline text-sky-500">Ibrahim Diallo</span></h1>
-                <h1>Référent(s): <span class="underline text-sky-500">Mohamed Buzyarsest</span></h1>
-              </div>
-              <div class="pl-[8px] rounded-[4px] bg-gray-300 text-gray-900">
-                <h1>Réf: <span class="font-black">PT-784</span></h1>
-                <h1>Date d'ouverture du dossier: <span class="font-black">29-05-204</span></h1>
-                <h1>Bénéficiaire(s) impliqué(s): <span class="underline text-sky-500">Benicio Del Toro</span>, <span class="underline text-sky-500">Lemaire Bertrand</span></h1>
-                <h1>Référent(s): <span class="underline text-sky-500">Mohamed Buzyarsest</span></h1>
-              </div>
-              <div class="pl-[8px] rounded-[4px]">
-                <h1>Réf: <span class="font-black">PT-784</span></h1>
-                <h1>Date d'ouverture du dossier: <span class="font-black">29-05-204</span></h1>
-                <h1>Bénéficiaire(s) impliqué(s): <span class="underline text-sky-500">Benicio Del Toro</span>, <span class="underline text-sky-500">Lemaire Bertrand</span></h1>
-                <h1>Référent(s): <span class="underline text-sky-500">Mohamed Buzyarsest</span></h1>
-              </div>
-              <div class="pl-[8px] rounded-[4px] bg-gray-300 text-gray-900">
-                <h1>Réf: <span class="font-black">PT-784</span></h1>
-                <h1>Date d'ouverture du dossier: <span class="font-black">29-05-204</span></h1>
-                <h1>Bénéficiaire(s) impliqué(s): <span class="underline text-sky-500">Benicio Del Toro</span>, <span class="underline text-sky-500">Lemaire Bertrand</span></h1>
-                <h1>Référent(s): <span class="underline text-sky-500">Mohamed Buzyarsest</span></h1>
-              </div>
-              <div class="pl-[8px] rounded-[4px]">
-                <h1>Réf: <span class="font-black">PT-784</span></h1>
-                <h1>Date d'ouverture du dossier: <span class="font-black">29-05-204</span></h1>
-                <h1>Bénéficiaire(s) impliqué(s): <span class="underline text-sky-500">Benicio Del Toro</span>, <span class="underline text-sky-500">Lemaire Bertrand</span></h1>
-                <h1>Référent(s): <span class="underline text-sky-500">Mohamed Buzyarsest</span></h1>
-              </div>
-              <div class="pl-[8px] rounded-[4px] bg-gray-300 text-gray-900">
-                <h1>Réf: <span class="font-black">PT-784</span></h1>
-                <h1>Date d'ouverture du dossier: <span class="font-black">29-05-204</span></h1>
-                <h1>Bénéficiaire(s) impliqué(s): <span class="underline text-sky-500">Benicio Del Toro</span>, <span class="underline text-sky-500">Lemaire Bertrand</span></h1>
-                <h1>Référent(s): <span class="underline text-sky-500">Mohamed Buzyarsest</span></h1>
-              </div>
-              <div class="pl-[8px] rounded-[4px]">
-                <h1>Réf: <span class="font-black">PT-784</span></h1>
-                <h1>Date d'ouverture du dossier: <span class="font-black">29-05-204</span></h1>
-                <h1>Bénéficiaire(s) impliqué(s): <span class="underline text-sky-500">Benicio Del Toro</span>, <span class="underline text-sky-500">Lemaire Bertrand</span></h1>
-                <h1>Référent(s): <span class="underline text-sky-500">Mohamed Buzyarsest</span></h1>
-              </div>
-              <div class="pl-[8px] rounded-[4px] bg-gray-300 text-gray-900">
-                <h1>Réf: <span class="font-black">PT-784</span></h1>
-                <h1>Date d'ouverture du dossier: <span class="font-black">29-05-204</span></h1>
-                <h1>Bénéficiaire(s) impliqué(s): <span class="underline text-sky-500">Benicio Del Toro</span>, <span class="underline text-sky-500">Lemaire Bertrand</span></h1>
-                <h1>Référent(s): <span class="underline text-sky-500">Mohamed Buzyarsest</span></h1>
-              </div>
-            </div>
-           
-            
-           
+        <div class="w-[100%] flex flex-1 gap-[8px] min-h-[0]">
+          <div class="p-[8px] bg-[#F6F6F6]/[0.5] rounded-[4px] basis-1/2 flex flex-col overflow-y-scroll cursor-pointer">
+              <For each={ParcoursTable}>
+                {(item, index) => {
+                  let IsIndexPair = (index() % 2) === 0 ? true : false;
+                  console.log(IsIndexPair);
+                  return (
+                  <div 
+                    classList={
+                      {
+                        "text-gray-900": CurrentSelectedParcoursId() !== item.id,
+                        "bg-gray-300": IsIndexPair && CurrentSelectedParcoursId() !== item.id,
+                        "bg-blue-800 text-blue-200": CurrentSelectedParcoursId() === item.id,
+                      }
+                    }
+                    onClick={() => SelectCurrentParcours(item.id)}
+                  >
+                    <h1>Réf: <span class="font-black">PC-478</span></h1>
+                    <h1>Date d'ouverture du dossier: <span class="font-black">{new Date(item.date * 1000).toUTCString()}</span></h1>
+                    <h1>Bénéficiaire(s) impliqué(s): <span class="underline text-sky-500">Amina Diallo</span>, <span class="underline text-sky-500">Ibrahim Diallo</span></h1>
+                    <h1>Référent(s): <span class="underline text-sky-500">Mohamed Buzyarsest</span></h1>
+                  </div>
+                )
+                }}
+              </For>
           </div>
-          <div class="bg-[#F6F6F6]/[0.5] rounded-[4px]  basis-1/2">I'm a surface material yeah.</div>
+          <div class="p-[8px] bg-[#F6F6F6]/[0.5] rounded-[4px]  basis-1/2">
+            <Show
+            when={CurrentSelectedParcoursId() !== null}
+            fallback={"Aucun parcours n'est séléctionné"}
+            >
+              <h1 class="text-[32px]">Action d'accompagnement réf {CurrentSelectedParcoursTable().ref}</h1>
+              <div class="flex gap-[8px]">
+                <button class="h-[32px] bg-[#FFFFFF]/[0.7] rounded-[3px] border-[1px] border-black/[0.16] text-[14px] min-w-[111px] px-[8px]">Filtrer</button>
+                <button class="h-[32px] bg-[#FFFFFF]/[0.7] rounded-[3px] border-[1px] border-black/[0.16] text-[14px] min-w-[111px] px-[8px]">Trier par</button>
+                <button class="h-[32px] bg-[#FFFFFF]/[0.7] rounded-[3px] border-[1px] border-black/[0.16] text-[14px] min-w-[111px] px-[8px]">Customiser les colonnes</button>
+                <input class=" flex-1 h-[32px] bg-[#FFFFFF]/[0.7] rounded-[3px] border-[1px] border-black/[0.16] text-[14px] min-w-[111px] px-[8px]"></input>
+              </div>
+              
+
+              <table class="text-left w-full">
+    <thead class="bg-black flex text-white w-full">
+      <tr class="flex w-full mb-4">
+        <th class="p-4 w-1/4">One</th>
+        <th class="p-4 w-1/4">Two</th>
+        <th class="p-4 w-1/4">Three</th>
+        <th class="p-4 w-1/4">Four</th>
+      </tr>
+    </thead>
+    <tbody class="bg-grey-light flex flex-col items-center justify-between overflow-y-scroll w-full" style="height: 50vh;">
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+      <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+            <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+            <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+            <tr class="flex w-full mb-4">
+        <td class="p-4 w-1/4">Dogs</td>
+        <td class="p-4 w-1/4">Cats</td>
+        <td class="p-4 w-1/4">Birds</td>
+        <td class="p-4 w-1/4">Fish</td>
+      </tr>
+    </tbody>
+  </table>
+
+
+            </Show>
+          </div>
         </div>
     </main>
   );
